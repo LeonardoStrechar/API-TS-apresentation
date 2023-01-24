d721b542da446063a2b05a5f94b6468e25a32c82
LeonardoStrechar <strechar21@gmail.com>
2023-01-24 00:09:40 -0300

    finish api

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const express_2 = require("express");
-const app = (0, express_1.default)();
+const zod_1 = require("zod");
 const route = (0, express_2.Router)();
+const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const port = 3001;
-route.get('/', (req, res) => {
-    res.json({ message: 'hello world with Typescript' });
+const TypesSchema = zod_1.z.object({
+    num1: zod_1.z.number(),
+    num2: zod_1.z.number()
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+route.post('/types', (req, res) => {
+    const result = TypesSchema.safeParse({
+        num1: req.body.num1,
+        num2: req.body.num2
+    });
+    //  if(result.success){
+    console.log(result);
+    //  }
 });
+app.use(route);
+app.listen(3001, () => 'server running on port 3001');
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,39 @@
 
-import express, { text } from 'express'
-
+import express from 'express'
 import { Router, Request, Response } from 'express';
-
-const app = express();
+import { z } from 'zod'
 
 const route = Router()
-
+const app = express()
 app.use(express.json())
 
-route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
+
+//É parecida com a Interface do TS
+const TypesSchema = z.object ({
+  num1: z.number(),
+  num2: z.number()
 })
 
+//Rota para receber os dados
 route.post('/types', (req: Request, res:Response) => {
-  const text = req.body.text
+  
+  //Validador de tipos
+  const result = TypesSchema.safeParse({
+    num1: req.body.num1,
+    num2: req.body.num2
+  })
 
-  if ( !text ){
-    console.log("é string")
-  }
-  console.log("não é string")
+  console.log(result)
 })
+
 
 app.use(route)
 
+app.listen(3001, () => 'server running on port 3001')
 
-app.listen(3001, () => 'server running on port 3333')
+
+// Comandos para rodar a aplicação
+
+// yarn install 
+// yarn run build 
+// yarn run dev
